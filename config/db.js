de3958b4b4d38ea206d363e44f40c408548932d6
ahmadammad908 <ahmadammad789@gmail.com>
2026-06-27 12:28:52 +0500

    Ahmad

--- config/db.js
+++ config/db.js
@@ -17,11 +17,11 @@ async function connectDB() {
             bufferCommands : false 
         }
 
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/ quickcart`, opts).then( mongoose => {
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then( mongoose => {
             return mongoose
         })
     }
     cached.conn = await cached.promise
     return cached.conn
 }
-export default connectDB
+export default connectDB  
